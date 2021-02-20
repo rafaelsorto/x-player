18e92154ca2e3d04bfec38c02aa8ee682dcce68b
Rafael Sorto <rafael@analogrepublic.com>
2021-02-20 11:42:59 -0600

    Added Player actions

--- src/store/features/player.ts
+++ src/store/features/player.ts
@@ -2,36 +2,32 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 import { RootState } from '../index'
 
 // Define a type for the slice state
-interface CounterState {
-  value: number
+interface PlayerState {
+  status: 'idle' | 'loading'
+  media: string
 }
 
 // Define the initial state using that type
-const initialState: CounterState = {
-  value: 0,
+const initialState: PlayerState = {
+  status: 'idle',
+  media: '',
 }
 
 export const counterSlice = createSlice({
-  name: 'counter',
+  name: 'player',
   // `createSlice` will infer the state type from the `initialState` argument
   initialState,
   reducers: {
-    increment: (state) => {
-      state.value += 1
-    },
-    decrement: (state) => {
-      state.value -= 1
-    },
-    // Use the PayloadAction type to declare the contents of `action.payload`
-    incrementByAmount: (state, action: PayloadAction<number>) => {
-      state.value += action.payload
+    play: (state, action: PayloadAction<string>) => {
+      state.media = action.payload
+      state.status = 'loading'
     },
   },
 })
 
-export const { increment, decrement, incrementByAmount } = counterSlice.actions
+export const { play } = counterSlice.actions
 
 // Other code such as selectors can use the imported `RootState` type
-export const selectCount = (state: RootState) => state.counter.value
+export const selectPlayer = (state: RootState): PlayerState => state.player
 
 export default counterSlice.reducer
