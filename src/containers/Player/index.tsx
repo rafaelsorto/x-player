78d8c5c3f647f662c5f270bdd2d5c7a9b024c180
Rafael Sorto <rafael@analogrepublic.com>
2021-02-21 13:26:16 -0600

    Added server.js

--- src/containers/Player/index.tsx
+++ src/containers/Player/index.tsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import { selectPlayer } from 'src/store/features/player'
 import { useSelector } from 'src/store/hooks'
+import ReactPlayer from 'react-player'
 
 export const Player: React.FC = () => {
   const { media, status } = useSelector(selectPlayer)
 
   return status !== 'idle' ? (
-    <iframe
-      src={`http://x-frame-player.url.sv/${encodeURIComponent(media)}`}
+    <ReactPlayer
+      url={media}
+      controls={true}
+      playing={false}
       height="100%"
-      width="40%"
+      width="100%"
     />
   ) : (
     <div>React Player</div>
