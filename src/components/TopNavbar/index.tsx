97f8baef1ab870d32d9a10c0e1b77bbd50547923
Rafael Sorto <rafael@analogrepublic.com>
2021-02-21 10:42:29 -0600

    Added Category Name

--- src/components/TopNavbar/index.tsx
+++ src/components/TopNavbar/index.tsx
@@ -1,5 +1,33 @@
 import React from 'react'
+import NextLink from 'next/link'
+import { Button, Flex } from '@chakra-ui/react'
+
+type LinkProps = {
+  href: string
+}
+
+const NavLink: React.FC<LinkProps> = ({ children, href, ...rest }) => (
+  <NextLink href={href} {...rest}>
+    <Button
+      as="a"
+      colorScheme="blue"
+      cursor="pointer"
+      minWidth="160px"
+      mr={2}
+      size="md"
+      variant="solid"
+    >
+      {children}
+    </Button>
+  </NextLink>
+)
 
 export const TopNavbar: React.FC = () => {
-  return <div>Top Navbar</div>
+  return (
+    <Flex>
+      <NavLink href="/live/categories">Live Streams</NavLink>
+      <NavLink href="/vod/categories">VOD</NavLink>
+      <NavLink href="/series/categories">Series</NavLink>
+    </Flex>
+  )
 }
--- src/containers/LiveStreams/index.tsx
+++ src/containers/LiveStreams/index.tsx
@@ -1,12 +1,13 @@
 import { Heading, Input, VStack } from '@chakra-ui/react'
 import { useNextQueryState } from 'src/hooks/generic/useNextQueryState'
-import { prop } from 'ramda'
-import React, { useCallback } from 'react'
+import { find, prop, propEq } from 'ramda'
+import React, { useCallback, useMemo } from 'react'
 import { ErrorMessage, FilterList, Loading } from 'src/components'
 import { filterStreams } from 'src/utils/filterStreams'
 import { useLiveStreams } from 'src/hooks/xtreamAPI/useLiveStreams'
 import { useRouter } from 'next/router'
 import { Item } from 'src/containers/LiveStreams/Item'
+import { useLiveStreamCategories } from 'src/hooks/xtreamAPI/useLiveStreamCategories'
 
 interface LiveStreamsProps {
   account: any
@@ -20,9 +21,20 @@ export const LiveStreams: React.FC<LiveStreamsProps> = ({ account }) => {
     password,
     server,
   })
+  const { data: categoryData } = useLiveStreamCategories({
+    username,
+    password,
+    server,
+  })
 
   const router = useRouter()
   const { categoryId }: { categoryId?: string } = router.query
+
+  const category = useMemo(() => {
+    return find(propEq('category_id', categoryId ?? ''))(
+      categoryData ?? []
+    ) as Category
+  }, [categoryData])
 
   const handleFilter = useCallback(
     (e) => {
@@ -45,7 +57,7 @@ export const LiveStreams: React.FC<LiveStreamsProps> = ({ account }) => {
 
   return (
     <VStack spacing="4" align="stretch">
-      <Heading color="blue.700">Live Streams</Heading>
+      <Heading color="blue.700">{category?.category_name}</Heading>
       <Input
         placeholder="Search Streams"
         onChange={handleFilter}
